0fadb89e7d6795d1658505a7b7ffad06d7177a80
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-11-30 10:36:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
-
+#include "lib.h"
+bool conversione_caratteri(char &a){
+  
+  bool t=true;
+  
+  if ((a>=65 && a<=90) || (a>=97 && a<=122)){
+    t=true;
+    if (a>=65 && a<=90){
+      a=a+32;
+      return a;
+    }else{
+      a=a-32;
+      return a;
+    }
+  }else{
+    t=false;
+  }
+  
+  return t; 
+}
