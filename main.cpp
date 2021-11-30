7d4849a76e095bf9d73b49fc8c7aace03c5bc8d3
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-11-30 10:40:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char a=a; 
+  cin >> a; 
+  
+  if (conversione_caratteri(a)== false){
+    cout << "errore" << endl;
+  }else{
+    cout << a << endl;
+  }
+  
   return 0;
 }
